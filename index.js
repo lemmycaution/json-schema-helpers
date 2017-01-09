d88cdf369e468b73197d79de300892ace696073b
Onur Uyar <me@onuruyar.com>
2017-01-09 17:15:47 +0000

    init

---
+++ index.js
@@ -0,0 +1,76 @@
+import _ from 'underscore'
+
+function findSchemaDefinition ($ref, definitions = {}) {
+  // Extract and use the referenced definition if we have it.
+  const match = /#\/definitions\/(.*)$/.exec($ref);
+  if (match && match[1] && definitions.hasOwnProperty(match[1])) {
+    return definitions[match[1]];
+  }
+  // No matching definition found, that's an error (bogus schema?)
+  throw new Error(`Could not find a definition for ${$ref}.`);
+}
+
+function parseSubSchema (subSchema, schema) {
+  if ( subSchema['properties'] ) {
+    subSchema['properties'] = parseSchema( subSchema, schema )
+
+  } else if ( subSchema['$ref'] ) {
+    subSchema = parseSchema( 
+      findSchemaDefinition( subSchema['$ref'], schema['definitions'] )
+    )
+
+  } else if ( subSchema['type'] === 'array' ) {
+    if (subSchema['items']['$ref']) {
+      subSchema['items'] = parseSchema(
+        findSchemaDefinition( subSchema['items']['$ref'], schema['definitions'] )
+      )
+    } else {
+      subSchema['items'] = parseSchema( subSchema['items'] )
+    }
+  } 
+  return subSchema
+}
+
+function parseSchema (orgSchema) {
+  let schema = JSON.parse(JSON.stringify(orgSchema))
+  if ( schema['properties'] ) {
+    _.each( schema['properties'], function (subSchema, subSchemaName) {
+      schema['properties'][subSchemaName] = parseSubSchema(subSchema, schema)
+    } )
+  } else {
+    schema = parseSubSchema(schema, schema)
+  }
+  return schema
+}
+
+function defineProperty (property, data, key, value) {
+  Object.defineProperty(data, key, {
+    enumerable: true,
+    configurable: false,
+    writable: !property.readOnly,
+    value: value
+  });
+  return data[key]
+}
+
+function defineProperties (schema, data) {
+  _.each(schema.properties, (property, key) => {
+    let value
+
+    switch (property.type) {
+      case "object":
+        value = defineProperties(property, data[key] || {})
+      break;
+      case "array":
+        value = data[key] || []
+      break;
+      default:
+        value = data[key]
+      break;
+    }
+    defineProperty(property, data, key, value)
+  })
+  return data
+}
+
+export {parseSchema, defineProperties}
---
+++ test/test.js
@@ -0,0 +1,112 @@
+import assert from 'assert'
+import { defineProperties, parseSchema } from '../index'
+
+
+describe('parseSchema', function() {
+  it('should normalize $ref in given schema', function() {
+    let schema = {
+      definitions: {
+        address: {
+          type: "object",
+          properties: {
+            city: {
+              type: 'string'
+            }
+          }
+        }
+      },
+      properties: {
+        billing_address: {
+          "$ref": "#/definitions/address"
+        },
+        shipping_addresses: {
+          type: "array",
+          items: {
+            "$ref": "#/definitions/address"
+          }
+        }
+      }
+    }
+    let parsedSchema = parseSchema(schema)
+    assert.equal(parsedSchema.properties.billing_address.properties.city.type, 'string')
+    assert.equal(parsedSchema.properties.shipping_addresses.items.properties.city.type, 'string')
+  })
+  it('should deal with complex schemas', function () {
+    let schema = require('./schema.json')
+    let parsedSchema = parseSchema(schema)
+    assert.equal(parsedSchema.properties.profile.properties.addresses.type, 'array')
+    assert.equal(parsedSchema.properties.profile.properties.addresses.items.properties.name.type, 'string')
+  })
+})
+
+describe('defineProperties', function() {
+  it('should generate primitive data types', function() {
+    let schema = {
+      properties: {
+        name: {
+          type: 'string'
+        },
+        age: {
+          type: 'number'
+        },
+        hasSchema: {
+          type: 'boolean'
+        }
+      }
+    }
+    assert.equal(defineProperties(schema, {name: 'my name'}).name, 'my name')
+    assert.equal(defineProperties(schema, {age: 12}).age, 12)
+    assert.equal(defineProperties(schema, {hasSchema: true}).hasSchema, true)
+    assert.equal(defineProperties(schema, {}).name, null)
+  })
+  it('should generate nested objects', function() {
+    let schema = {
+      properties: {
+        location: {
+          type: 'object',
+          properties: {
+            map: {
+              type: 'string'
+            },
+            point: {
+              type: 'object',
+              properties: {
+                lat: {
+                  type: 'number'
+                },
+                lon: {
+                  type: 'number'
+                },
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    assert.equal(defineProperties(schema, {location: {point: {lat: 1}}}).location.point.lat, 1)
+    assert.equal(defineProperties(schema, {location: {map: 'google'}}).location.map, 'google')
+    assert.equal(defineProperties(schema, {}).location.map, null)
+    assert.equal(defineProperties(schema, {}).location.point.lat, null)
+  })
+  it('should generate arrays', function() {
+    let schema = {
+      properties: {
+        classes: {
+          type: 'array',
+          items: {
+            type: 'text'
+          }
+        }
+      }
+    }
+    
+    assert.equal(defineProperties(schema, {classes: ['A1']}).classes[0], 'A1')
+    assert.equal(defineProperties(schema, {}).classes.length, 0)
+  })
+  it('should deal with complex schemas', function () {
+    let schema = require('./schema.json')
+    let parsedSchema = parseSchema(schema)
+    assert.equal(defineProperties(parsedSchema, {}).profile.emails.length, 0)
+  })
+})
